49b0481f936572e2af604ca20defc073c0cb6723
Hrithik <slumdog4321@gmail.com>
2023-12-20 00:14:03 +0530

    side navbar added

--- app/(dashBoard)/_components/SideNav.js
+++ app/(dashBoard)/_components/SideNav.js
@@ -1,5 +1,7 @@
-import { Upload } from "lucide";
-import React from "react";
+"use client";
+import { File, Shield, Upload } from "lucide";
+import Image from "next/image";
+import React, { useState } from "react";
 
 function SideNav() {
   const menuList = [
@@ -7,22 +9,42 @@ function SideNav() {
       id: 1,
       name: "Upload",
       icon: Upload,
-      path: "upload",
+      path: "/upload",
     },
     {
       id: 2,
-      name: "Upload",
-      icon: Upload,
-      path: "upload",
+      name: "Files",
+      icon: File,
+      path: "/files",
     },
     {
       id: 3,
-      name: "Upload",
-      icon: Upload,
-      path: "upload",
+      name: "Upgrade",
+      icon: Shield,
+      path: "/upgrade",
     },
   ];
-  return <div>SideNav</div>;
+
+  const [activeIndex, setActiveIndex] = useState(0);
+  return (
+    <div className="shadow-sm h-full border-r">
+      <div className="p-5 border-b">
+        <Image src="/logo.svg" width={150} height={100} />
+      </div>
+      <div className="flex flex-col float-left">
+        {menuList.map((item, index) => (
+          <button
+            className={`flex gap-2 p-4 px-0 hover:bg-gray-100 text-gray-500 
+            ${activeIndex == index ? "bg-blue-50 text-primary" : null}`}
+            onClick={() => setActiveIndex(index)}
+          >
+            <item.icon />
+            <h2>{item.name}</h2>
+          </button>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default SideNav;
