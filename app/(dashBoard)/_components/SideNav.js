5832400e29f99a837f77721e3ecd8cbb1019f6b1
Hrithik <slumdog4321@gmail.com>
2023-12-19 23:58:56 +0530

    side navbar created and icon added via lucid

---
+++ app/(dashBoard)/_components/SideNav.js
@@ -0,0 +1,28 @@
+import { Upload } from "lucide";
+import React from "react";
+
+function SideNav() {
+  const menuList = [
+    {
+      id: 1,
+      name: "Upload",
+      icon: Upload,
+      path: "upload",
+    },
+    {
+      id: 2,
+      name: "Upload",
+      icon: Upload,
+      path: "upload",
+    },
+    {
+      id: 3,
+      name: "Upload",
+      icon: Upload,
+      path: "upload",
+    },
+  ];
+  return <div>SideNav</div>;
+}
+
+export default SideNav;
--- app/(dashBoard)/layout.js
+++ app/(dashBoard)/layout.js
@@ -1,10 +1,13 @@
 import React from "react";
+import SideNav from "./_components/SideNav";
 
 function layout({ children }) {
   return (
     <div>
-      Home
-      {children}
+      <div className="hidden h-full md:w-64 flex-col fixed inset-y-0 z-50">
+        <SideNav />
+      </div>
+      <div className="md:ml-64">{children}</div>
     </div>
   );
 }
