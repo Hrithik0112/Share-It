a7f1dcc1fc321fd977fedb0c6cca60682cecff9c
Hrithik <slumdog4321@gmail.com>
2023-12-20 00:22:51 +0530

    Top Header added

---
+++ app/(dashBoard)/_components/TopHeader.js
@@ -0,0 +1,16 @@
+import { UserButton } from "@clerk/nextjs";
+import { AlignJustify } from "lucide";
+import Image from "next/image";
+import React from "react";
+
+function TopHeader() {
+  return (
+    <div className="flex p-5 border-b justify-center md:justify-end">
+      <AlignJustify className="md:hidden" />
+      <Image src="/logo,svg" width={150} height={100} className="md:hidden" />
+      <UserButton />
+    </div>
+  );
+}
+
+export default TopHeader;
--- app/(dashBoard)/layout.js
+++ app/(dashBoard)/layout.js
@@ -1,13 +1,17 @@
 import React from "react";
 import SideNav from "./_components/SideNav";
+import TopHeader from "./_components/TopHeader";
 
 function layout({ children }) {
   return (
     <div>
-      <div className="hidden h-full md:w-64 flex-col fixed inset-y-0 z-50">
+      <div className=" h-full w-64 flex-col fixed inset-y-0 z-50 md:flex hidden">
         <SideNav />
       </div>
-      <div className="md:ml-64">{children}</div>
+      <div className="md:ml-64">
+        <TopHeader />
+        {children}
+      </div>
     </div>
   );
 }
