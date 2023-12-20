e85e83b298a24f7c48bc86770f9930b0025305f4
Hrithik <slumdog4321@gmail.com>
2023-12-20 11:36:41 +0530

    upload Form Ui added

--- app/(dashBoard)/(routes)/upload/page.js
+++ app/(dashBoard)/(routes)/upload/page.js
@@ -1,7 +1,16 @@
 import React from "react";
+import UploadForm from "./_components/UploadForm";
 
 function Upload() {
-  return <div>Upload</div>;
+  return (
+    <div className="p-5 px-8 md:px-28">
+      <h2 className="text-[20px] m-5 text-center">
+        Start <span className="text-primary">Uplaoding</span> file And{" "}
+        <span className="text-primary">Share</span> It.
+      </h2>
+      <UploadForm />
+    </div>
+  );
 }
 
 export default Upload;
