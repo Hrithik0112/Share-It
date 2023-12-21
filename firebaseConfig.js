04d0f99b1c21c150e460f527b28f2ab4e5946dce
Hrithik <slumdog4321@gmail.com>
2023-12-21 13:53:34 +0530

    firebase config setup

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDNXXBC7XHOJyjAHMfxGxXVsczdT7SGugM",
+  authDomain: "file-sharing-app-4a0d7.firebaseapp.com",
+  projectId: "file-sharing-app-4a0d7",
+  storageBucket: "file-sharing-app-4a0d7.appspot.com",
+  messagingSenderId: "89774552206",
+  appId: "1:89774552206:web:0066dbf1b36585d42bc5df",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
