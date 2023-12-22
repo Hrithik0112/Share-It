b4dac864e1d6c58a5f41a76c9e68f77a704de44a
Hrithik <slumdog4321@gmail.com>
2023-12-22 10:04:11 +0530

    get the download url from firebase storage

--- app/(dashBoard)/(routes)/upload/page.js
+++ app/(dashBoard)/(routes)/upload/page.js
@@ -3,7 +3,7 @@
 import React from "react";
 import UploadForm from "./_components/UploadForm";
 import { app } from "@/firebaseConfig";
-import { getStorage, ref, uploadBytesResumable } from "firebase/storage";
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage";
 
 function Upload({ uploadBtnclick }) {
   const storage = getStorage(app);
@@ -12,11 +12,16 @@ function Upload({ uploadBtnclick }) {
       contentType: file.type,
     };
     const storageRef = ref(storage, "File-Upload/" + file?.name);
-    const uploadTask = uploadBytesResumable(storageRef, file, metadata);
+    const uploadTask = uploadBytesResumable(storageRef, file, file.type);
     uploadTask.on("state_changed", (snapshot) => {
       // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
       const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
       console.log("Upload is " + progress + "% done");
+
+      progress == 100 &&
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          console.log("File available at", downloadURL);
+        });
     });
   };
   return (
